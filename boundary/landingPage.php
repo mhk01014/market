bceb2bd4ebd8892ecd6fb9c6db54a3eacffe1239
Mya Hmue <myahmue80@gmail.com>
2025-06-09 04:45:48 +0800

    Boundary folder and files created

---
+++ boundary/landingPage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Tailwind CDN Example</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+  <h1 class="text-4xl font-bold text-blue-600">
+    Hello Tailwind via CDN!
+  </h1>
+</body>
+</html>
